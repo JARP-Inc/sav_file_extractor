491b5ea59ac4092fe63faaf91ca39985526de383
Rory McDermid <2566302M@student.gla.ac.uk>
2023-03-25 23:25:06 +0000

    here you go

---
+++ extractor.py
@@ -0,0 +1,5 @@
+from zipfile import ZipFile
+
+
+def extractor():
+    with ZipFile("C:\Users\Rory New PC\JARP\.sav_file_extractor\test_saves", r) as zObject:
---
+++ finder.py
@@ -0,0 +1,10 @@
+import glob
+
+# put the name of your save location in the brackets
+def finder():
+    # return glob.glob("./test_saves/*.sav")
+    print(glob.glob("C:/Users/Rory New PC/Documents/Paradox Interactive/Stellaris/save games/game/*"))
+
+
+# if __name__ == "__main__":
+    # finder()
